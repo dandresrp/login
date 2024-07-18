d7a81bb086226c99e06ff95830121e9a2c1629cf
rob_sanz <robertosanchez@unah.hn>
2024-07-18 09:17:07 -0600

    pagina-registro rediseño

--- lib/pantalla_registro.dart
+++ lib/pantalla_registro.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
-class PantallaRegistro extends StatelessWidget {
+class PantallaRegistro extends StatefulWidget {
   const PantallaRegistro({super.key});
+
+  @override
+  State<PantallaRegistro> createState() => _PantallaRegistroState();
+}
+
+class _PantallaRegistroState extends State<PantallaRegistro> {
+
+  bool ocultarContrasenia1 = true;
+  bool ocultarContrasenia2 = true;
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,8 +35,9 @@ class PantallaRegistro extends StatelessWidget {
 
               margin: EdgeInsets.all(30),
               padding: EdgeInsets.all(20),
-              height: 1000,
-              child: Column(
+              height: 800,
+              child: 
+              Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Divider(color: Colors.white,),
@@ -41,103 +53,78 @@ class PantallaRegistro extends StatelessWidget {
                   SizedBox(height:55,),
 
                   //NOMBRE DE USUARIO
-                  Row(
-                    children: const [
-                      Icon(Icons.person),
-                      SizedBox(width: 5,),
-                      Text('Nombre de Usuario', style: 
-                        TextStyle              
-                        (
-                          color: Color.fromRGBO(24, 24, 24, 1),
-                          fontSize: 15, 
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
-                  ),
                   Form(
-                    child: TextFormField()
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Nombre de Usuario', 
+                        icon: Icon(Icons.person,size: 25,)
+                      ),
+                    )
                   ),
                   SizedBox(height:45,),
 
                   //CORREO ELECTRONICO
-                  Row(
-                    children: const [
-                      Icon(Icons.mail),
-                      SizedBox(width: 5,),
-                      Text('Correo Electronico', style: 
-                        TextStyle
-                        (
-                          color: Color.fromRGBO(24, 24, 24, 1),
-                          fontSize: 15, 
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
-                  ),
                   Form(
-                    child: TextFormField()
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Correo Electronico',
+                        icon: Icon(Icons.mail,size: 25,) 
+                      ),
+                    )
                   ),
                   SizedBox(height:45,),
                   
                   //TELEFONO
-                  Row(
-                    children: const [
-                      Icon(Icons.phone),
-                      SizedBox(width: 5,),
-                      Text('Telefono', style: 
-                        TextStyle
-                        (
-                          color: Color.fromRGBO(24, 24, 24, 1),
-                          fontSize: 15, 
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
-                  ),
                   Form(
-                    child: TextFormField()
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Telefono',
+                        icon: Icon(Icons.phone,size: 25,) 
+                      ),
+                    )
                   ),
                   SizedBox(height:45,),
 
                   //CONTRASEÑA 
-                  Row(
-                    children: const [
-                      Icon(Icons.password),
-                      SizedBox(width: 5,),
-                      Text('Contraseña', style: 
-                        TextStyle
-                        (
-                          color: Color.fromRGBO(24, 24, 24, 1),
-                          fontSize: 15, 
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
-                  ),
                   Form(
-                    child: TextFormField(obscureText: true,)
+                    child: TextFormField(
+                      obscureText: ocultarContrasenia1,
+                      decoration: InputDecoration(
+                        labelText: 'Contraseña', 
+                        icon: Icon(Icons.key,size: 25,),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            ocultarContrasenia1 = !ocultarContrasenia1;
+                            setState(() {
+                            });
+                          },
+                          icon: Icon(ocultarContrasenia1?  Icons.visibility : Icons.visibility_off),
+                        )
+                      ),
+                      
+                      )
                   ),
                   SizedBox(height:45,),
 
                   //CONFIRMAR CONTRASEÑA
-                  Row(
-                    children: const [
-                      Icon(Icons.password),
-                      SizedBox(width: 5,),
-                      Text('Confirmar Contraseña', style: 
-                        TextStyle
-                        (
-                          color: Color.fromRGBO(24, 24, 24, 1),
-                          fontSize: 15, 
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ],
-                  ),
                   Form(
-                    child: TextFormField(obscureText: true)
-                  ),SizedBox(height:45,),
+                    child: TextFormField(
+                      obscureText: ocultarContrasenia2,
+                      decoration: InputDecoration(
+                        labelText: 'Confirmar Contraseña',
+                        icon: Icon(Icons.key,size: 25,),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            ocultarContrasenia2 = !ocultarContrasenia2;
+                            setState(() {
+                            });
+                          },
+                          icon: Icon(ocultarContrasenia2?  Icons.visibility : Icons.visibility_off),
+                        )
+                        
+                      ),
+                      )
+                  ),
                   SizedBox(height:55,),
 
                   //Botones
@@ -145,7 +132,7 @@ class PantallaRegistro extends StatelessWidget {
                     children: [
                       ElevatedButton(
                         onPressed: () {
-                          Navigator.of(context).pushNamed('home');
+                          Navigator.pushReplacementNamed(context,'home');
                         },
                         child: Text('Registrarse')
                         ),
@@ -156,6 +143,7 @@ class PantallaRegistro extends StatelessWidget {
                       TextButton(
                         onPressed: () {
                           Navigator.of(context).pushNamed('iniciarSesion');
+                          setState(() {                          });
                         },
                         child: Text('¿Ya tienes una cuenta?'),
                       )
