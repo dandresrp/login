ebd75ab6d30e498dc8e0f8c992dca1821b86ac53
rob_sanz <robertosanchez@unah.hn>
2024-07-18 01:23:57 -0600

    Molde pantalla tegistro

--- lib/pantalla_registro.dart
+++ lib/pantalla_registro.dart
@@ -6,16 +6,79 @@ class PantallaRegistro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          color: Color.fromRGBO(54, 194, 206, 1),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: const [
-            SizedBox(height: 60,),
-              Text('REGISTRATE', style: TextStyle(color: Color.fromRGBO(24, 24, 24, 1), fontSize: 20, fontWeight: FontWeight.bold),)
-                  
-            ],
+      body: Container(
+        color: Color.fromRGBO(119, 228, 200, 150),
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(54, 194, 206, 60),
+              borderRadius: BorderRadius.circular(10.0)
+            ),
+            margin: EdgeInsets.all(30),
+            padding: EdgeInsets.all(20),
+            height: 600,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 60,),
+                Text('REGISTRATE', style: 
+                  TextStyle
+                  (
+                    color: Color.fromRGBO(24, 24, 24, 1),
+                    fontSize: 20, 
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height:55,),
+                Text('Correo Electroncio', style: 
+                  TextStyle
+                  (
+                    color: Color.fromRGBO(24, 24, 24, 1),
+                    fontSize: 20, 
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField()
+                    ],
+                  )
+                ),
+                SizedBox(height:45,),
+                Text('Nombre de Usuario', style: 
+                  TextStyle
+                  (
+                    color: Color.fromRGBO(24, 24, 24, 1),
+                    fontSize: 20, 
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField()
+                    ],
+                  )
+                ),
+                SizedBox(height:45,),
+                Text('Contraseña', style: 
+                  TextStyle
+                  (
+                    color: Color.fromRGBO(24, 24, 24, 1),
+                    fontSize: 20, 
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField()
+                    ],
+                  )
+                ),
+              ],
+            ),
           ),
         ),
       ),
